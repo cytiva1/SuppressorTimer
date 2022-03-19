ee68d954693a22b787bd1c0c0165da0af870da5d
cytiva1 <101941995+cytiva1@users.noreply.github.com>
2022-03-19 11:06:24 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,27 @@
 let counterEl = document.getElementById('timePassed');
 
+let facts = [
+        "Russia has invaded Ukraine",
+        "Texas enacted a law to ban abortion after 6 weeks of gestation"
+]
+
 function myFunc() {
     let time = Date.now() - 1617914597267;
     let seconds = Math.floor(time / 1000);
     let minutes = Math.floor(seconds / 60);
     let hours = Math.floor(minutes / 60);
     let days = Math.floor(hours / 24);
-    // setTimeout(() => {
-    //     myFunc();
-    // }, 1000)
+    let moonOrbitsApprox = Math.floor(days / 27.32)
+    
     counterEl.innerText = `Time Taco has spent waiting for a suppressor...
     Days: ${days}
     Hours: ${hours} 
     Minutes: ${minutes} 
-    Seconds: ${seconds}`;
+    Seconds: ${seconds}
+    
+    Fun Facts:
+    Since then the moon has orbited earth about ${moonOrbitsApprox} times.
+    `;
 }
 
 setInterval(myFunc, 1000);
