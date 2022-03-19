2041d6a30b5183672abbe84dc8c2632be4ff1223
cytiva1 <101941995+cytiva1@users.noreply.github.com>
2022-03-19 11:35:08 -0600

    Update script.js

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ let counterEl = document.getElementById('timePassed');
 
 function myFunc() {
     let time = Date.now() - 1617914597267;
-    let seconds = Math.floor(time / 1000);
-    let minutes = Math.floor(seconds / 60);
-    let hours = Math.floor(minutes / 60);
-    let days = (hours / 24).toFixed(2);
-    let moonOrbitsApprox = (days / 27.32);
+    let seconds = Math.floor(time / 1000).toLocaleString();
+    let minutes = Math.floor(seconds / 60).toLocaleString();
+    let hours = Math.floor(minutes / 60).toLocaleString();
+    let days = (hours / 24).toFixed(2).toLocaleString();
+    let moonOrbitsApprox = (days / 27.32).toLocaleString();
     
     counterEl.innerText = `Time Taco has spent waiting for a suppressor...
     Days: ${days}
