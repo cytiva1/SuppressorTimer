e96df1a62fe3c4d3120d2559e505689bd96f5d07
cytiva1 <101941995+cytiva1@users.noreply.github.com>
2022-04-08 11:14:31 -0600

    Suppressors came in, can make static now.

--- script.js
+++ script.js
@@ -1,19 +1,16 @@
 let counterEl = document.getElementById('timePassed');
 
-function myFunc() {
-    let time = Date.now() - 1617914597267;
-    let seconds = Math.floor(time / 1000);
-    let minutes = Math.floor(seconds / 60);
-    let hours = Math.floor(minutes / 60);
-    let days = (hours / 24).toFixed(2);
-    let moonOrbitsApprox = (days / 27.32);
-    
-    counterEl.innerText = `Time Taco has spent waiting for a suppressor...
-    Days: ${days.toLocaleString()}
-    Hours: ${hours.toLocaleString()} 
-    Minutes: ${minutes.toLocaleString()} 
-    Seconds: ${seconds.toLocaleString()}
-    `;
-}
 
-setInterval(myFunc, 1000);
+let time = Date.getTime('April 8, 2022 20:17:40 GMT+00:00') - 1617914597267;
+let seconds = Math.floor(time / 1000);
+let minutes = Math.floor(seconds / 60);
+let hours = Math.floor(minutes / 60);
+let days = (hours / 24).toFixed(2);
+let moonOrbitsApprox = (days / 27.32);
+
+counterEl.innerText = `Taco waited this long for his suppressors...
+Days: ${days.toLocaleString()}
+Hours: ${hours.toLocaleString()} 
+Minutes: ${minutes.toLocaleString()} 
+Seconds: ${seconds.toLocaleString()}
+`;
