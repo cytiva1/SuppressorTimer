2440000ad95284d044fd6b6749bf2cade3479648
cytiva1 <101941995+cytiva1@users.noreply.github.com>
2022-05-21 12:56:22 -0600

    Update script.js

--- script.js
+++ script.js
@@ -2,21 +2,25 @@ let counterEl = document.getElementById('timePassed');
 
 let d = new Date('May 18, 22 12:29:00 GMT+00:00');
 
-while(true){
-  setTimeout(() => {
-    let now = new Date();
-    let time = now.getTime() - d.getTime() ;
-    let seconds = Math.floor(time / 1000);
-    let minutes = Math.floor(seconds / 60);
-    let hours = Math.floor(minutes / 60);
-    let days = (hours / 24).toFixed(2);
-    let moonOrbitsApprox = (days / 27.32);
+function updateTime() = {
+  let now = new Date();
+  let time = now.getTime() - d.getTime() ;
+  let seconds = Math.floor(time / 1000);
+  let minutes = Math.floor(seconds / 60);
+  let hours = Math.floor(minutes / 60);
+  let days = (hours / 24).toFixed(2);
+  let moonOrbitsApprox = (days / 27.32);
 
-    counterEl.innerText = `Taco waited this long for his suppressors...
-    Days: ${days.toLocaleString()}
-    Hours: ${hours.toLocaleString()} 
-    Minutes: ${minutes.toLocaleString()} 
-    Seconds: ${seconds.toLocaleString()}
-    `;
+  counterEl.innerText = `Taco waited this long for his suppressors...
+  Days: ${days.toLocaleString()}
+  Hours: ${hours.toLocaleString()} 
+  Minutes: ${minutes.toLocaleString()} 
+  Seconds: ${seconds.toLocaleString()}
+  `;
+}
+
+while(true){
+  setTimeout(
+    updateTime();
   }, 1000)
 }
