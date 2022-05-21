199e37943955223ca17f78fb798f11e4e4ded00e
cytiva1 <101941995+cytiva1@users.noreply.github.com>
2022-05-21 12:47:30 -0600

    Updated for new order

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
 let counterEl = document.getElementById('timePassed');
 
-let d = new Date('April 7, 22 21:27:40 GMT+00:00');
-let time = d.getTime() - 1617914597267;
-let seconds = Math.floor(time / 1000);
-let minutes = Math.floor(seconds / 60);
-let hours = Math.floor(minutes / 60);
-let days = (hours / 24).toFixed(2);
-let moonOrbitsApprox = (days / 27.32);
+let d = new Date('May 18, 22 12:29:00 GMT+00:00');
 
-counterEl.innerText = `Taco waited this long for his suppressors...
-Days: ${days.toLocaleString()}
-Hours: ${hours.toLocaleString()} 
-Minutes: ${minutes.toLocaleString()} 
-Seconds: ${seconds.toLocaleString()}
-`;
+setTimeout(() => {
+  let now = new Date();
+  let time = now.getTime() - d.getTime() ;
+  let seconds = Math.floor(time / 1000);
+  let minutes = Math.floor(seconds / 60);
+  let hours = Math.floor(minutes / 60);
+  let days = (hours / 24).toFixed(2);
+  let moonOrbitsApprox = (days / 27.32);
+
+  counterEl.innerText = `Taco waited this long for his suppressors...
+  Days: ${days.toLocaleString()}
+  Hours: ${hours.toLocaleString()} 
+  Minutes: ${minutes.toLocaleString()} 
+  Seconds: ${seconds.toLocaleString()}
+  `;
+}, "1000")
+
