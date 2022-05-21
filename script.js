813033649e5a5558cea5714117ebec0e1f7c6307
cytiva1 <101941995+cytiva1@users.noreply.github.com>
2022-05-21 13:05:24 -0600

    Update script.js

--- script.js
+++ script.js
@@ -3,8 +3,9 @@ let counterEl = document.getElementById('timePassed');
 let d = new Date('May 18, 22 12:29:00 GMT+00:00');
 
 function updateTime() {
+
   let now = new Date();
-  let time = now.getTime() - d.getTime() ;
+  let time = now.getTime() - d.getTime();
   let seconds = Math.floor(time / 1000);
   let minutes = Math.floor(seconds / 60);
   let hours = Math.floor(minutes / 60);
